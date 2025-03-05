d35f8bc17361d581ff6fa8abcd57aa5c6a21b6f9
Kacperon <kacper.feliks.455@gmail.com>
2025-03-05 20:54:08 +0100

    app fix

--- app.py
+++ app.py
@@ -1,47 +1,55 @@
+from fastapi import FastAPI
 from class444 import Game_class
 from minmax_alfa_beta import MinimaxabPlayer
 from minmax import MinimaxPlayer
 from monte_carlo_time import MonteCarloPlayer
 
+app = FastAPI()
 
-def start_game(n: int) -> list:
+@app.post("/start_game")
+def start_game(n: int):
     "return the map of the game as a list of lists of triples"
     "where first element is a player number and the second contains card value"
     game = [[None for _ in range(n)] for _ in range(n)]
-    if n<=4:
+    if n <= 4:
         game[0][0] = (1, 1)
         game[n-1][n-1] = (2, 1)
     else:
         game[1][1] = (1, 1)
         game[n-2][n-2] = (2, 1)
-    return game
+    return {"game": game}
 
-def get_valid_moves(tab: list,player: int) -> list:
+@app.post("/get_valid_moves")
+def get_valid_moves(tab: list, player: int):
     "return a list of valid moves for the current player"
-    Game=Game_class(0)
-    Game.import_game(tab,player)
-    return Game.get_position()[Game.current_player-1]
+    Game = Game_class(0)
+    Game.import_game(tab, player)
+    return {"valid_moves": Game.get_position()[Game.current_player-1]}
 
-def make_move(tab: list, move: tuple, player) -> tuple[list, int]:
+@app.post("/make_move")
+def make_move(tab: list, move: tuple, player: int):
     "make a move for the current player and return the new map and the winner or 0 if the game is not finished"
-    Game=Game_class(0)
-    Game.import_game(tab,player)
-    tab=Game.play_visual(move)
-    winer=Game.get_winner()
-    return tab,winer
+    Game = Game_class(0)
+    Game.import_game(tab, player)
+    tab = Game.play_visual(move)
+    winner = Game.get_winner()
+    return {"new_game_state": tab, "winner": winner}
 
-def generate_move_minmax(game:list,deph: int,if_alphabeta: bool) -> tuple:
+@app.post("/generate_move_minmax")
+def generate_move_minmax(game: list, depth: int, if_alphabeta: bool):
     "return the best move for the current player using Minmax algorithm"
-    Game=Game_class(0)
-    Game.import_game(game,1)
+    Game = Game_class(0)
+    Game.import_game(game, 1)
     if if_alphabeta:
-        return MinimaxabPlayer(deph).find_best_move(Game,Game.current_player)
+        best_move = MinimaxabPlayer(depth).find_best_move(Game, Game.current_player)
     else:
-        return MinimaxPlayer(deph).find_best_move(Game,Game.current_player)
-    
-def generate_move_monte_carlo(game: list,time: int) -> tuple:
-    "return the best move for the current player using Monte Carlo algorithm"
-    Game=Game_class(0)
-    Game.import_game(game,1)
-    return MonteCarloPlayer(time).find_best_move(Game,Game.current_player)
+        best_move = MinimaxPlayer(depth).find_best_move(Game, Game.current_player)
+    return {"best_move": best_move}
 
+@app.post("/generate_move_monte_carlo")
+def generate_move_monte_carlo(game: list, time: int):
+    "return the best move for the current player using Monte Carlo algorithm"
+    Game = Game_class(0)
+    Game.import_game(game, 1)
+    best_move = MonteCarloPlayer(time).find_best_move(Game, Game.current_player)
+    return {"best_move": best_move}
